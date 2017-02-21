ea2795af59a21d2fbb806202a9975ad21bf81b3c
zbw001091 <13817682773@163.com>
2017-02-21 16:07:41 +0800

    16:07

--- SpringBootSample01/src/main/java/Example.java
+++ SpringBootSample01/src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 	@RequestMapping("/")
 	String home() {
 		return "Hello World!";
-	}
+	} 
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Example.class, args);
